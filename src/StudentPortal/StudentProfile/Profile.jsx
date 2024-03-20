c753de00b6de4174cf0991a7b98431296e32b4b0
Varad Dhumale <varaddhumale177@gmail.com>
2024-03-21 02:47:31 +0530

    profile page

--- src/StudentPortal/StudentProfile/Profile.jsx
+++ src/StudentPortal/StudentProfile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import toast from "react-hot-toast";
 import api from "../../Api/Api";
@@ -18,6 +18,24 @@ function Profile() {
       { company: "", role: "", duration: "" }, // Initial empty experience object
     ],
   });
+
+  const [user, setUser] = useState()
+
+  useEffect(() => {
+
+    const id = sessionStorage.getItem('_id')
+    const _id = JSON.parse(id)
+    try {
+      api.post('/get-user-by-id', { _id }).then((res) => {
+        setUser(res.data)
+      }).catch(err => {
+        console.log(err.message);
+      })
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [])
 
   const [editOn, setEditOn] = useState(true);
 
@@ -136,7 +154,7 @@ function Profile() {
     <>
       <Navbar bgColor="profile" />
 
-      {/* <div className="p-4 sm:ml-64">
+      <div className="p-4 sm:ml-64">
         <div className="container mx-auto py-8">
           <form onSubmit={handleSubmit} className="max-w-lg mx-auto">
 
@@ -282,238 +300,9 @@ function Profile() {
             </button>
           </form>
         </div>
-      </div> */}
-
-      <div style={{ marginLeft: "22rem", padding: "2rem" }}>
-        <h2 className="mb-8 text-4xl font-bold leadiF sm:text-5xl">Profile</h2>
-
-        <section className="flex" style={{ backgroundColor: "white", border: "1px solid rgba(0, 0, 0, 0.19)", borderRadius: "12px" }}>
-          <div style={{ padding: "2rem", backgroundColor: " #f1f1f8", width: "auto" }}>
-            <div style={{ backgroundColor: "grey", width: "12rem", height: "12rem", borderRadius: "50%", marginBottom: "2rem" }}></div>
-            <div  style={{width: "fit-content", margin: "0 auto" }}>
-              <h1 style={{fontSize: "2rem", fontWeight: 500, marginBottom: "1rem"}}>Skills</h1>
-              <h4 className="mb-1">MongoDB</h4>
-              <h4 className="mb-1">Express JS</h4>
-              <h4 className="mb-1">React JS</h4>
-              <h4 className="mb-1">Node JS</h4>
-            </div>
-          </div>
-
-          {!editOn && (
-          <div id="editOn" style={{ padding: "2rem" }}>
-            <h4 style={{ fontSize: "2rem" }}>Account Details</h4>
-
-
-            <form class="max-w-sm mx-auto">
-
-              <div className="mb-2">
-                <label htmlFor="branch" className="block mb-1">Select Branch:</label>
-                <select
-                  id="branch"
-                  onChange={handleChange}
-                  name="branch"
-                  value={info.branch}
-                  className="border rounded py-2 px-3 w-full"
-                >
-                  <option value="">Select Branch</option>
-                  {branchOptions.map((branch, index) => (
-                    <option key={index} value={branch}>{branch}</option>
-                  ))}
-                </select>
-              </div>
-
-              <div className="flex items-center mb-2">
-                <select
-                  multiple
-                  value={info.selectedSkills}
-                  onChange={handleSkillsChange}
-                  className="border rounded py-2 px-3 mr-2 w-1/2"
-                >
-
-                  {hardcodedSkills.map((skill, index) => (
-                    <option key={index} value={skill}>{skill}</option>
-                  ))}
-                </select>
-                <button
-                  type="button"
-                  onClick={handleAddSkill}
-                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-                >
-                  Add Skills
-                </button>
-              </div>
-
-              <div className="mb-4">
-                {info.skills.map((skill, index) => (
-                  <span
-                    key={index}
-                    className="bg-gray-200 text-gray-700 py-1 px-2 rounded-full mr-2 mb-2 inline-block"
-                  >
-                    {skill}
-                    <button
-                      type="button"
-                      onClick={() => handleRemoveSkill(skill)}
-                      className="ml-2 focus:outline-none"
-                    >
-                      &#10005;
-                    </button>
-                  </span>
-                ))}
-              </div>
-
-              {info.experiences.map((experience, index) => (
-                <div key={index} className="flex flex-col mb-4">
-                  <input
-                    type="text"
-                    name="company"
-                    value={experience.company}
-                    onChange={(e) => handleExperienceChange(index, e)}
-                    placeholder="Company"
-                    className="border rounded py-2 px-3 mb-2"
-                  />
-                  <input
-                    type="text"
-                    name="role"
-                    value={experience.role}
-                    onChange={(e) => handleExperienceChange(index, e)}
-                    placeholder="Role"
-                    className="border rounded py-2 px-3 mb-2"
-                  />
-                  <input
-                    type="text"
-                    name="duration"
-                    value={experience.duration}
-                    onChange={(e) => handleExperienceChange(index, e)}
-                    placeholder="Duration"
-                    className="border rounded py-2 px-3 mb-2"
-                  />
-                  <button
-                    type="button"
-                    onClick={() => handleRemoveExperience(index)}
-                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-full inline-block"
-                  >
-                    Remove
-                  </button>
-                </div>
-              ))}
-
-              <button
-                type="button"
-                onClick={handleAddExperience}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
-              >
-                Add Experience
-              </button>
-
-              <button
-                type="submit"
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full"
-              >
-                Update Profile
-              </button>
-
-
-            </form>
-
-          </div>
-          )}
-
-          {editOn && (
-          <div id="editOff">
-          <div style={{padding: "1rem 1rem 0 1rem", width: "100%"}}>
-            <h1 style={{fontSize: "2rem", marginBottom: "1rem", fontWeight: 500}}>Personal Information</h1>
-
-            <div className="flex">
-              <div className="left" style={{marginRight: "5rem"}}>
-                <div className="box">
-                  <h3 className="beforeDetail">First Name</h3>
-                  <h3 className="afterDetail">Farhaan</h3>
-                </div>
-                <div className="box">
-                  <h3 className="beforeDetail">Email Address</h3>
-                  <h3 className="afterDetail">farhaan8d@gmail.com</h3>
-                </div>
-                <div className="box">
-                  <h3 className="beforeDetail">Branch</h3>
-                  <h3 className="afterDetail">Computer Science</h3>
-                </div>
-              </div>
-              <div className="right">
-              <div className="box">
-                  <h3 className="beforeDetail">Last Name</h3>
-                  <h3 className="afterDetail">Shaikh</h3>
-                </div>
-                <div className="box">
-                  <h3 className="beforeDetail">Phone</h3>
-                  <h3 className="afterDetail">7021177410</h3>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div style={{padding: "0 1rem 1rem 1rem", width: "100%"}}>
-            <h1 style={{fontSize: "2rem", marginBottom: "1rem", fontWeight: 500}}>Experience</h1>
-
-            <div className="grid grid-cols-3">
-            <div style={{marginRight: "5rem" }} className="box">
-              <h3 className="beforeDetail">Company</h3>
-              <h3 className="afterDetail">ABC Company</h3>
-            </div>
-            <div style={{marginRight: "5rem" }} className="box">
-              <h3 className="beforeDetail">Role</h3>
-              <h3 className="afterDetail">Web Developer</h3>
-            </div>
-            <div className="box">
-              <h3 className="beforeDetail">Duration</h3>
-              <h3 className="afterDetail">Jan 2023 - Jan 2024</h3>
-            </div>
-
-            </div>
-            <div className="grid grid-cols-3">
-            <div style={{marginRight: "5rem" }} className="box">
-              <h3 className="beforeDetail">Company</h3>
-              <h3 className="afterDetail">DEF Company</h3>
-            </div>
-            <div style={{marginRight: "5rem" }} className="box">
-              <h3 className="beforeDetail">Role</h3>
-              <h3 className="afterDetail">SDE</h3>
-            </div>
-            <div className="box">
-              <h3 className="beforeDetail">Duration</h3>
-              <h3 className="afterDetail">Aug 2023 - Aug 2024</h3>
-            </div>
-
-            </div>
-            <div className="grid grid-cols-3">
-            <div style={{marginRight: "5rem" }} className="box">
-              <h3 className="beforeDetail">Company</h3>
-              <h3 className="afterDetail">XYZ Company</h3>
-            </div>
-            <div style={{marginRight: "5rem" }} className="box">
-              <h3 className="beforeDetail">Role</h3>
-              <h3 className="afterDetail">Android Developer</h3>
-            </div>
-            <div className="box">
-              <h3 className="beforeDetail">Duration</h3>
-              <h3 className="afterDetail">April 2023 - April 2024</h3>
-            </div>
-            </div>
-          </div>
-          </div>
-          )}
-
-
-          
-          <svg onClick={handleEditClick} style={{width: "50px", height: "50px", transform: "rotate(90deg)"}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
-          <path d="M20.84 7.48l-4.88-4.88a2 2 0 0 0-2.83 0l-1.59 1.59L16.59 9l1.59-1.59a.5.5 0 0 1 .71 0l4.89 4.89a.5.5 0 0 1 0 .7l-1.59 1.59a.5.5 0 0 1-.71 0l-4.89-4.89a.5.5 0 0 1 0-.71L15.17 6l1.59-1.59a2 2 0 0 0 0-2.83z"></path>
-          {/* <line x1="4" y1="20" x2="16" y2="20"></line> */}
-         </svg>
-
-
-        </section>
-
-
       </div>
+
+    
     </>
   );
 }
