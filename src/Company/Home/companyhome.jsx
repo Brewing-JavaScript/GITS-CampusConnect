93d020f9b36356a229bfa975a2b7b2b68d87f441
Farhaan <farhaan8d@gmail.com>
2024-03-21 13:05:49 +0530

    Viewing resume

--- src/Company/Home/companyhome.jsx
+++ src/Company/Home/companyhome.jsx
@@ -22,8 +22,10 @@ const CompanyHomePage = () => {
     // Adjust the width of the iframe based on its content width
     const adjustIframeWidth = () => {
       if (iframeRef.current) {
-        const contentWidth = iframeRef.current.contentWindow.document.body.scrollWidth;
-        iframeRef.current.style.width = `${contentWidth}px`;
+        // const contentWidth = iframeRef.current.contentWindow.document.body.scrollWidth;
+        // iframeRef.current.style.width = `${contentWidth}px - 200px`;
+
+        
       }
     };
 
@@ -60,7 +62,13 @@ const CompanyHomePage = () => {
   };
 
   const viewResume = (resumeUrl) => {
+    
     setSelectedResumeUrl(resumeUrl);
+    const iframeDoc = iframeRef.current.contentWindow.document;
+        const images = iframeDoc.getElementsByTagName('img');
+        for (let i = 0; i < images.length; i++) {
+            images[i].style.width = '70%';
+        }
   };
 
   const closeResumeModal = () => {
@@ -85,7 +93,7 @@ const CompanyHomePage = () => {
 
   const handleCall = (email) => {
 
-  navigate(`/company-call/${email}`)
+    navigate(`/company-call/${email}`)
   }
 
   return (
@@ -99,156 +107,156 @@ const CompanyHomePage = () => {
             </div>
           </nav> */}
 
-        <aside
-        style={{
-          backgroundColor: "rgba(255, 255, 255, 0)",
-          padding: "1.3rem",
-          width: "20%",
-        }}
-        id="default-sidebar"
-        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-        aria-label="Sidebar"
-      >
-        <div
-          style={{ border: "1px solid #00000030", borderRadius: "20px" }}
-          class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800"
-        >
-          <h1
+          <aside
             style={{
-              color: "black",
-              fontWeight: 700,
-              textAlign: "center",
-              marginBottom: "1rem",
-              fontSize: "1.3rem",
+              backgroundColor: "rgba(255, 255, 255, 0)",
+              padding: "1.3rem",
+              width: "20%",
             }}
+            id="default-sidebar"
+            class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+            aria-label="Sidebar"
           >
-            CampusConnect
-          </h1>
-
-          <ul class="space-y-2 font-medium">
-            <li
-              // style={{background: "rgb(31, 31, 31)", borderRadius: "10px"}}
-              style={{
-                background: "rgb(31, 31, 31)",
-                borderRadius: "10px",
-              }}
-              className="hover:bg-gray-200"
+            <div
+              style={{ border: "1px solid #00000030", borderRadius: "20px" }}
+              class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800"
             >
-              <a
-                href="/company"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white  "
+              <h1
+                style={{
+                  color: "black",
+                  fontWeight: 700,
+                  textAlign: "center",
+                  marginBottom: "1rem",
+                  fontSize: "1.3rem",
+                }}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="white"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9z"></path>
-                  <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                </svg>
+                CampusConnect
+              </h1>
 
-                <span
+              <ul class="space-y-2 font-medium">
+                <li
+                  // style={{background: "rgb(31, 31, 31)", borderRadius: "10px"}}
                   style={{
-                    fontSize: "1.3rem",
-                    color: "white",
+                    background: "rgb(31, 31, 31)",
+                    borderRadius: "10px",
                   }}
-                  className="ms-3"
+                  className="hover:bg-gray-200"
                 >
-                  Home
-                </span>
-              </a>
-            </li>
+                  <a
+                    href="/company"
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white  "
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="white"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    >
+                      <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9z"></path>
+                      <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                    </svg>
 
-            <li
-              // style={{background: "rgb(31, 31, 31)", borderRadius: "10px"}}
-              style={{
-                borderRadius: "10px",
-              }}
-              className="hover:bg-gray-200"
-            >
-              <a
-                href="/company"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white  "
-              >
-                  <svg
+                    <span
+                      style={{
+                        fontSize: "1.3rem",
+                        color: "white",
+                      }}
+                      className="ms-3"
+                    >
+                      Home
+                    </span>
+                  </a>
+                </li>
+
+                <li
+                  // style={{background: "rgb(31, 31, 31)", borderRadius: "10px"}}
                   style={{
-                    width: "1.3rem",
-                    height: "1.3rem",
+                    borderRadius: "10px",
                   }}
-                  className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 21"
+                  className="hover:bg-gray-200"
                 >
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                </svg>
+                  <a
+                    href="/company"
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white  "
+                  >
+                    <svg
+                      style={{
+                        width: "1.3rem",
+                        height: "1.3rem",
+                      }}
+                      className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 22 21"
+                    >
+                      <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                      <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                    </svg>
 
-                <span
+                    <span
+                      style={{
+                        fontSize: "1.3rem",
+                      }}
+                      className="ms-3"
+                    >
+                      Top Applicants
+                    </span>
+                  </a>
+                </li>
+
+                <li
                   style={{
-                    fontSize: "1.3rem",
+                    borderRadius: "10px",
                   }}
-                  className="ms-3"
+                  className="hover:bg-gray-200"
                 >
-                  Top Applicants
-                </span>
-              </a>
-            </li>
-
-            <li
-              style={{
-                borderRadius: "10px",
-              }}
-              className="hover:bg-gray-200"
-            >
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white  "
-              >
-                <svg
-                  style={{
-                    width: "1.3rem",
-                    height: "1.3rem",
-                  }}
-                  className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 16"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
-                  />
-                </svg>
-                <span
-                  style={{
-                    fontSize: "1.3rem",
-                    color: "#535050",
-                  }}
-                  onClick={handleSignOut}
-                  className="flex-1 ms-3 whitespace-nowrap"
-                >
-                  Sign Out
-                </span>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </aside>
+                  <a
+                    href="#"
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white  "
+                  >
+                    <svg
+                      style={{
+                        width: "1.3rem",
+                        height: "1.3rem",
+                      }}
+                      className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 18 16"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
+                      />
+                    </svg>
+                    <span
+                      style={{
+                        fontSize: "1.3rem",
+                        color: "#535050",
+                      }}
+                      onClick={handleSignOut}
+                      className="flex-1 ms-3 whitespace-nowrap"
+                    >
+                      Sign Out
+                    </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </aside>
 
 
-          <div className="w-3/4 p-6" style={{marginLeft: "22rem"}}>
+          <div className="w-3/4 p-6" style={{ marginLeft: "22rem" }}>
             {/* <button onClick={toggleNavbar} className="block mb-4 bg-gray-900 text-white py-2 px-4 rounded">
               {isOpen ? 'Close' : 'Open'}
             </button> */}
@@ -322,28 +330,29 @@ const CompanyHomePage = () => {
       </div>
       {selectedResumeUrl && (
         <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 overflow-hidden">
-        <div className="absolute w-full h-full cursor-pointer bg-gray-900 bg-opacity-75" onClick={closeResumeModal}></div>
-        <div className="flex items-center justify-center w-full h-full">
-          <div style={{ width: '50%', maxHeight: '80vh' }} className="bg-white rounded-lg cursor-pointer p-8 relative overflow-y-auto">
-            <button onClick={closeResumeModal} className="absolute top-0 right-0 m-4 text-gray-600 hover:text-gray-800">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-              </svg>
-            </button>
-            <div>
+          <div className="absolute w-full h-full cursor-pointer bg-gray-900 bg-opacity-75" onClick={closeResumeModal}></div>
+          <div className="flex items-center justify-center w-full h-full">
+            <div style={{ width: '50%', }} className="bg-white rounded-lg cursor-pointer p-8 relative overflow-y-auto">
+              <button onClick={closeResumeModal} className="absolute top-0 right-0 m-4 text-gray-600 hover:text-gray-800">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                </svg>
+              </button>
+              <div>
                 <iframe
                   title="Resume"
                   ref={iframeRef}
                   src={selectedResumeUrl}
-                  className="w-full"
-                  style={{ minHeight: '400px' }} // Set min height if needed
+                  style={{ width: "100%", height: "50vh" }}
                 />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      
-      
+
+
+
+
       )}
     </div>
   );
