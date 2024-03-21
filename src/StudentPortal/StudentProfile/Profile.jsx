02ad1b6e1b3a2cf9b54de4cb4527096aa6c03feb
Farhaan <farhaan8d@gmail.com>
2024-03-21 10:51:09 +0530

    Edited Profile

--- src/StudentPortal/StudentProfile/Profile.jsx
+++ src/StudentPortal/StudentProfile/Profile.jsx
@@ -19,7 +19,7 @@ function Profile() {
     ],
   });
 
-  const [user, setUser] = useState()
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
 
@@ -154,158 +154,184 @@ function Profile() {
     <>
       <Navbar bgColor="profile" />
 
-      <div className="p-4 sm:ml-64">
-        <div className="container mx-auto py-8">
-          <form onSubmit={handleSubmit} className="max-w-lg mx-auto">
+      <div style={{marginLeft: "22rem", paddingTop: "2rem"}}>
+        <div className="container mx-auto" style={{height: "auto"}}>
+          <form style={{display: "flex", width: "100%"}} onSubmit={handleSubmit} className=" mx-auto">
 
-            <div className="flex items-center justify-center">
-              <input
-                onChange={getImage}
-                type="file"
-                id="upload_banner"
-                accept=".jpeg, .png , .jpg"
-                className="hidden"
-              />
-              <label htmlFor="upload_banner">
-                <img
-                  src={info.avatar ? URL.createObjectURL(info.avatar) : "https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Free-Image.png"}
-                  alt="Avatar"
-                  className="w-32 h-32 bg-gray-300 rounded-full cursor-pointer"
+            <div className="left" style={{marginRight: "3rem"}}>
+              <div className="flex">
+                <input
+                  onChange={getImage}
+                  type="file"
+                  id="upload_banner"
+                  accept=".jpeg, .png , .jpg"
+                  className="hidden"
                 />
-              </label>
-            </div>
+                <label htmlFor="upload_banner">
+                  <img style={{width:"15rem", height: "15rem"}}
+                    src={info.avatar ? URL.createObjectURL(info.avatar) : "https://www.pngall.com/wp-content/uploads/5/Profile-Avatar-PNG-Free-Image.png"}
+                    alt="Avatar"
+                    className="w-32 h-32 bg-gray-300 rounded-full cursor-pointer"
+                  />
+                </label>
+              </div>
 
-            <div className="flex justify-center mt-4">
-              <button
-                onClick={handleUploadClick}
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                Upload Resume
-              </button>
-            </div>
-
-            <textarea
-              onChange={handleChange}
-              name="heading"
-              type="text"
-              value={info.heading}
-              className="border rounded py-2 px-3 mb-2 mt-4 w-full"
-              placeholder="Enter The Event Heading"
-            />
-
-            <div className="mb-2">
-              <label htmlFor="branch" className="block mb-1">Select Branch:</label>
-              <select
-                id="branch"
-                onChange={handleChange}
-                name="branch"
-                value={info.branch}
-                className="border rounded py-2 px-3 w-full"
-              >
-                <option value="">Select Branch</option>
-                {branchOptions.map((branch, index) => (
-                  <option key={index} value={branch}>{branch}</option>
-                ))}
-              </select>
-            </div>
-
-            <div className="flex items-center mb-2">
-              <select
-                multiple
-                value={info.selectedSkills}
-                onChange={handleSkillsChange}
-                className="border rounded py-2 px-3 mr-2 w-1/2"
-              >
-
-                {hardcodedSkills.map((skill, index) => (
-                  <option key={index} value={skill}>{skill}</option>
-                ))}
-              </select>
-              <button
-                type="button"
-                onClick={handleAddSkill}
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-              >
-                Add Skills
-              </button>
-            </div>
-
-            <div className="mb-4">
-              {info.skills.map((skill, index) => (
-                <span
-                  key={index}
-                  className="bg-gray-200 text-gray-700 py-1 px-2 rounded-full mr-2 mb-2 inline-block"
+              <div className="flex justify-center mt-4">
+                <button style={{padding: '1rem 2rem'}}
+                  onClick={handleUploadClick}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
-                  {skill}
-                  <button
-                    type="button"
-                    onClick={() => handleRemoveSkill(skill)}
-                    className="ml-2 focus:outline-none"
-                  >
-                    &#10005;
-                  </button>
-                </span>
-              ))}
-            </div>
-
-            {info.experiences.map((experience, index) => (
-              <div key={index} className="flex flex-col mb-4">
-                <input
-                  type="text"
-                  name="company"
-                  value={experience.company}
-                  onChange={(e) => handleExperienceChange(index, e)}
-                  placeholder="Company"
-                  className="border rounded py-2 px-3 mb-2"
-                />
-                <input
-                  type="text"
-                  name="role"
-                  value={experience.role}
-                  onChange={(e) => handleExperienceChange(index, e)}
-                  placeholder="Role"
-                  className="border rounded py-2 px-3 mb-2"
-                />
-                <input
-                  type="text"
-                  name="duration"
-                  value={experience.duration}
-                  onChange={(e) => handleExperienceChange(index, e)}
-                  placeholder="Duration"
-                  className="border rounded py-2 px-3 mb-2"
-                />
-                <button
-                  type="button"
-                  onClick={() => handleRemoveExperience(index)}
-                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-full inline-block"
-                >
-                  Remove
+                  Upload Resume
                 </button>
               </div>
-            ))}
+            </div>
 
-            <button
-              type="button"
-              onClick={handleAddExperience}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
-            >
-              Add Experience
-            </button>
+            <div className="right" style={{width: "50%"}}>
+            <h2 className="mb-4 text-4xl font-bold leadiF sm:text-5xl">
+              Personal Information
+            </h2>
 
-            <button
-              type="submit"
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full"
-            >
-              Update Profile
-            </button>
+              <textarea
+                onChange={handleChange}
+                name="heading"
+                type="text"
+                value={info.heading}
+                className="border rounded py-2 px-3 mb-2 w-full"
+                placeholder="Enter The Event Heading"
+              />
+
+              <div className="mb-2">
+                <label htmlFor="branch" className="block mb-1">Select Branch:</label>
+                <select
+                  id="branch"
+                  onChange={handleChange}
+                  name="branch"
+                  value={info.branch}
+                  className="border rounded py-2 px-3 w-full"
+                >
+                  <option value="">Select Branch</option>
+                  {branchOptions.map((branch, index) => (
+                    <option key={index} value={branch}>{branch}</option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="skillsAndExperiences flex">
+                <div className="section-skills mb-2" style={{marginRight: "5rem"}}>
+                  <h2 className="mb-4 mt-4 text-4xl font-bold leadiF sm:text-5xl">
+                    Skills
+                  </h2>
+                  <select
+                    // multiple
+                    value={info.selectedSkills}
+                    onChange={handleSkillsChange}
+                    className="border rounded"
+                  >
+
+                    {hardcodedSkills.map((skill, index) => (
+                      <option key={index} value={skill}>{skill}</option>
+                    ))}
+                  </select>
+                  <div className="mb-4 mt-4">
+                    {info.skills.map((skill, index) => (
+                      <span
+                      style={{padding: "0.5rem 1rem"}}
+                        key={index}
+                        className="bg-gray-200 text-gray-700 py-1 px-2 rounded-full mb-2 block"
+                      >
+                        {skill}
+                        <button
+                        style={{padding: " 0 0", marginLeft: "5px"}}
+                          type="button"
+                          onClick={() => handleRemoveSkill(skill)}
+                          className=" focus:outline-none"
+                        >
+                          &#10005;
+                        </button>
+                      </span>
+                    ))}
+                  </div>
+
+
+                  <button
+                    type="button"
+                    onClick={handleAddSkill}
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4"
+                  >
+                    Add Skills
+                  </button>
+                </div>
+
+                <div className="section-exp" style={{width: "100%"}}>
+                  <h2 className="mb-4 mt-4 text-4xl font-bold leadiF sm:text-5xl">
+                    Experiences
+                  </h2>
+
+                  
+                
+
+                  {info.experiences.map((experience, index) => (
+                    <div key={index} className="flex flex-col mb-4">
+                      <input
+                        type="text"
+                        name="company"
+                        value={experience.company}
+                        onChange={(e) => handleExperienceChange(index, e)}
+                        placeholder="Company"
+                        className="border rounded py-2 px-3 mb-2"
+                      />
+                      <input
+                        type="text"
+                        name="role"
+                        value={experience.role}
+                        onChange={(e) => handleExperienceChange(index, e)}
+                        placeholder="Role"
+                        className="border rounded py-2 px-3 mb-2"
+                      />
+                      <input
+                        type="text"
+                        name="duration"
+                        value={experience.duration}
+                        onChange={(e) => handleExperienceChange(index, e)}
+                        placeholder="Duration"
+                        className="border rounded py-2 px-3 mb-2"
+                      />
+                      <button
+                        type="button"
+                        onClick={() => handleRemoveExperience(index)}
+                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded-full inline-block"
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  ))}
+
+                  <button
+                    type="button"
+                    onClick={handleAddExperience}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+                  >
+                    Add Experience
+                  </button>
+
+                  <button
+                    type="submit"
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full"
+                  >
+                    Update Profile
+                  </button>
+
+                </div> {/* Experience */}
+
+              </div> {/* skillsAndExperiences */}
+
+            </div>
+
           </form>
         </div>
       </div>
-
-    
     </>
   );
 }
 
 export default Profile;
-
